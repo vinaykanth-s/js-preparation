3de3d72d1db9dade9d65e0d013ccf88aad88928f
Vinay Kanth <vinaykanth.edu@gmail.com>
2025-02-27 22:22:51 +0530

    added a promise based output question

---
+++ JSConcepts/output-based-questions/p1.js
@@ -0,0 +1,13 @@
+//q1
+const promise = new Promise(res => res(2));
+
+promise.then(v => {
+    console.log(v)
+    return v*2
+}).then(v => {
+    console.log(v)
+    return v*2
+}).finally(v => {
+    console.log(v)
+    return v*2
+}).then(v => console.log(v))
